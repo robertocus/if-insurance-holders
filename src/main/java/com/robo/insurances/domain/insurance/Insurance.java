b06b9b02a582fe3b75a86fd5e1fcbe257866255d
robo <robert.jansson@gmail.com>
2025-06-30 10:28:31 +0200

    more test

--- src/main/java/com/robo/insurances/domain/insurance/Insurance.java
+++ src/main/java/com/robo/insurances/domain/insurance/Insurance.java
@@ -3,11 +3,10 @@ package com.robo.insurances.domain.insurance;
 import com.robo.insurances.domain.vehicle.RegistrationNumber;
 
 public interface Insurance {
-    static final CarInsurance HOLDER_1_CAR_INSURANCE = CarInsurance.builder().holderId(HolderId.HOLDER_1).vehicleId(RegistrationNumber.ABC).build();
-    static final HealthInsurance HOLDER_1_HEALTH_INSURANCE = HealthInsurance.builder().holderId(HolderId.HOLDER_1).build();
-    static final PetInsurance HOLDER_1_PET_INSURANCE = PetInsurance.builder().holderId(HolderId.HOLDER_1).build();
-    static final CarInsurance HOLDER_2_CAR_INSURANCE = CarInsurance.builder().holderId(HolderId.HOLDER_2).vehicleId(RegistrationNumber.DEF).build();
-
+    CarInsurance HOLDER_1_CAR_INSURANCE = CarInsurance.builder().holderId(HolderId.HOLDER_1).vehicleId(RegistrationNumber.ABC).build();
+    HealthInsurance HOLDER_1_HEALTH_INSURANCE = HealthInsurance.builder().holderId(HolderId.HOLDER_1).build();
+    PetInsurance HOLDER_1_PET_INSURANCE = PetInsurance.builder().holderId(HolderId.HOLDER_1).build();
+    CarInsurance HOLDER_2_CAR_INSURANCE = CarInsurance.builder().holderId(HolderId.HOLDER_2).vehicleId(RegistrationNumber.DEF).build();
 
     Integer monthlyDollarCost();
 
--- src/test/java/com/robo/insurances/domain/insurance/InsuranceFinderTest.java
+++ src/test/java/com/robo/insurances/domain/insurance/InsuranceFinderTest.java
@@ -1,17 +1,63 @@
 package com.robo.insurances.domain.insurance;
 
+import com.robo.insurances.domain.Features;
+import com.robo.insurances.domain.vehicle.RegistrationNumber;
+import com.robo.insurances.domain.vehicle.Vehicle;
+import com.robo.insurances.domain.vehicle.VehicleClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
 class InsuranceFinderTest {
+    private InsuranceFinder insuranceFinder;
+    private InsuranceRepository insuranceRepositoryMock;
+    private VehicleClient vehicleClientMock;
+    private Features features;
 
     @BeforeEach
     void setUp() {
-
+        insuranceRepositoryMock = mock(InsuranceRepository.class);
+        vehicleClientMock = mock(VehicleClient.class);
+        features = new Features(false);
+        insuranceFinder = new InsuranceFinder(insuranceRepositoryMock, vehicleClientMock, features);
     }
 
     @Test
-    void findByHolder() {
+    void findByHolderNoFeatureAndNothingFound() {
+        when(insuranceRepositoryMock.findByHolder(HolderId.HOLDER_1)).thenReturn(List.of());
+        assertThat(insuranceFinder.findByHolder(HolderId.HOLDER_1)).isEqualTo(List.of());
+        verify(vehicleClientMock, times(0)).find(any());
+    }
 
+    @Test
+    void findByHolderNoFeature2Found() {
+        when(insuranceRepositoryMock.findByHolder(HolderId.HOLDER_1)).thenReturn(List.of(Insurance.HOLDER_1_CAR_INSURANCE, Insurance.HOLDER_1_PET_INSURANCE));
+        assertThat(insuranceFinder.findByHolder(HolderId.HOLDER_1)).isEqualTo(List.of(Insurance.HOLDER_1_CAR_INSURANCE, Insurance.HOLDER_1_PET_INSURANCE));
+        verify(vehicleClientMock, times(0)).find(any());
+    }
+
+    @Test
+    void findByHolderWithFeature2FoundNoVehicleInsurance() {
+        features.setIncludeVehicleInfo(true);
+        when(insuranceRepositoryMock.findByHolder(HolderId.HOLDER_1)).thenReturn(List.of(Insurance.HOLDER_1_HEALTH_INSURANCE, Insurance.HOLDER_1_PET_INSURANCE));
+        assertThat(insuranceFinder.findByHolder(HolderId.HOLDER_1)).isEqualTo(List.of(Insurance.HOLDER_1_HEALTH_INSURANCE, Insurance.HOLDER_1_PET_INSURANCE));
+        verify(vehicleClientMock, times(0)).find(any());
+    }
+
+    @Test
+    void findByHolderWithFeature2FoundWithVehicleInsurance() {
+        features.setIncludeVehicleInfo(true);
+        var carInsuranceWithVehicle = CarInsuranceWithVehicleInfo.builder().holderId(HolderId.HOLDER_1).vehicle(Vehicle.create(RegistrationNumber.ABC, "Volvo")).build();
+
+        when(insuranceRepositoryMock.findByHolder(HolderId.HOLDER_1)).thenReturn(List.of(Insurance.HOLDER_1_CAR_INSURANCE, Insurance.HOLDER_1_PET_INSURANCE));
+        when(vehicleClientMock.find(RegistrationNumber.ABC)).thenReturn(Optional.of(carInsuranceWithVehicle.getVehicle()));
+
+        assertThat(insuranceFinder.findByHolder(HolderId.HOLDER_1)).isEqualTo(List.of(Insurance.HOLDER_1_PET_INSURANCE, carInsuranceWithVehicle));
+        verify(vehicleClientMock, times(1)).find(any());
     }
 }
